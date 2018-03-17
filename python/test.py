bcd8b92d2346a9758292e5be8ee162a712940fef
hopkira <hopkira@googlemail.com>
2018-03-17 19:15:30 +0000

    More missing ) !

--- python/test.py
+++ python/test.py
@@ -4,9 +4,9 @@ from roboclaw import Roboclaw
 rc = Roboclaw("/dev/roboclaw",115200)
 rc.Open()
 address = 0x80
-clicks = 1000
-click_vel = 150
-ACCELERATION = 300
+clicks = 300
+click_vel = 30
+ACCELERATION = 10
 rc.SpeedAccelDistanceM1M2(address=address,accel=ACCELERATION,speed1=click_vel,distance1=int(abs(clicks/2)),speed2=int(click_vel),distance2=int(abs(clicks/2)),buffer=1)
 rc.SpeedAccelDistanceM1M2(address=address,accel=ACCELERATION,speed1=0,distance1=int(abs(clicks/2)),speed2=0,distance2=int(abs(clicks/2)),buffer=0)
 buffers = (0,0,0)
