ec3dbaad652213dfe2c29e1571ac2859ecea171d
hopkira <hopkira@googlemail.com>
2018-03-31 16:32:08 +0100

    Adding snowy

--- python/mic-speech-text-stream.py
+++ python/mic-speech-text-stream.py
@@ -6,7 +6,7 @@ from watson_developer_cloud import SpeechToTextV1
 from watson_developer_cloud.websocket import RecognizeCallback
 from watson_developer_cloud import ConversationV1
 
-import os, sys, subprocess, threading, time, json, re
+import os, sys, subprocess, threading, time, json, re, signal, snowboydecoder
 
 STTusername = os.environ['WTTSusername']
 STTpassword = os.environ['WTTSpassword']
@@ -31,6 +31,35 @@ print ("STT Password: " + str(STTpassword))
 print ("WA Username: " + str(WAusername))
 print ("WA Password: " + str(WApassword))
 print ("WA Workspace: " + str(WAworkspace_id))
+
+# Initialise snowboy global variables and methods
+model = "./K9.pmdl"
+interrupted = False
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+def interrupt_callback():
+    global interrupted
+    return interrupted
+
+signal.signal(signal.SIGINT, signal_handler)
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+
+# K9 hotword has been detected
+def K9_detected():
+    global pwm
+    print "K9 hotword detected...\n"
+    set_PWM(PWM_eye,30)
+    global stop_now
+    stop_now = True # get the detector to terminate
+
+def stop_snowboy():
+    global stop_now
+    if stop_now = True:
+        print "Snowboy stop interrupt."
+    return stop_now
 
 # Example using websockets
 class MyRecognizeCallback(RecognizeCallback):
@@ -61,23 +90,32 @@ class MyRecognizeCallback(RecognizeCallback):
         transcript = hypothesis
         print(transcript)
 
-finished = False
-try:
-    os.remove("my_voice.wav")
-except OSError:
-    pass
-mycallback = MyRecognizeCallback()
-record = "arecord -d 5 -f S16_LE -r 44100 -t wav my_voice.wav"
-print("Lights on")
-p = subprocess.call(record, shell=True)
-print ("Lights off")
-with open('my_voice.wav') as f:
-    speech_to_text.recognize_with_websocket(audio=f,content_type='audio/l16; rate=44100', recognize_callback=mycallback)
-while not finished:
-    time.sleep(0.1)
-response = conversation.message(workspace_id=WAworkspace_id, input={'text':transcript})
-results = re.search(': \{u\'text\': \[u\'(.*)\'\], u\'log', str(response))
-answer = results.group(1)
-answer = './tts ' + answer
-print (str(answer))
-subprocess.call(answer, shell=True)
+go = True
+while go:
+    interrupted = False
+    stop_now = False
+    finished = False
+    print "Listening for K9 keyword... press Ctrl+C to exit"
+    detector.start(detected_callback=K9_detected,
+    interrupt_check=stop_snowboy,
+    sleep_time=0.03)
+    detector.terminate()
+    time.sleep(0.03)
+    try:
+        os.remove("my_voice.wav")
+    except OSError:
+        pass
+    mycallback = MyRecognizeCallback()
+    record = "arecord -d 5 -f S16_LE -r 44100 -t wav my_voice.wav"
+    print("Lights on")
+    p = subprocess.call(record, shell=True)
+    print ("Lights off")
+    with open('my_voice.wav') as f:
+        speech_to_text.recognize_with_websocket(audio=f,content_type='audio/l16; rate=44100', recognize_callback=mycallback)
+    while not finished:
+        time.sleep(0.1)
+    response = conversation.message(workspace_id=WAworkspace_id, input={'text':transcript})
+    results = re.search(': \{u\'text\': \[u\'(.*)\'\], u\'log', str(response))
+    answer = results.group(1)
+    answer = './tts ' + answer
+    subprocess.call(answer, shell=True)
