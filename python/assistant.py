6f8306c6a1552286813310163330d33f589010d4
hopkira <hopkira@googlemail.com>
2018-04-01 18:00:41 +0100

    Test for timeout

--- python/assistant.py
+++ python/assistant.py
@@ -68,8 +68,8 @@ class SpeechToTextClient(WebSocketClient):
     def opened(self):
         print "opened(self) and self.send"
         self.send('{"action": "start", "content-type": "audio/l16;rate=16000"}')
-        self.stream_audio_thread = threading.Thread(target=self.stream_audio)
-        self.stream_audio_thread.start()
+        #self.stream_audio_thread = threading.Thread(target=self.stream_audio)
+        #self.stream_audio_thread.start()
 
     def received_message(self, message):
         global speech_received
