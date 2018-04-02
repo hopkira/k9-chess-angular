6bb539664142cc06055dc359fe86637769ab72e1
hopkira <hopkira@googlemail.com>
2018-04-02 14:13:10 +0100

    Debugging (again!)

--- python/assistant.py
+++ python/assistant.py
@@ -67,7 +67,7 @@ class SpeechToTextClient(WebSocketClient):
 
     def opened(self):
         print "opened(self) and self.send"
-        self.send('{"action":"start","content-type":"audio/l16;rate=16000","inactivity_timeout":1,"interim_results":true}')
+        self.send('{"action":"start","content-type":"audio/l16;rate=16000","interim_results":true}')
         self.stream_audio_thread = threading.Thread(target=self.stream_audio)
         self.stream_audio_thread.start()
     def received_message(self, message):
@@ -82,6 +82,7 @@ class SpeechToTextClient(WebSocketClient):
                 set_PWM(PWM_eye,100)
                 print "Speak now..."
         if "results" in message:
+            print "Got here!"
             response = json.loads(message)
             print response.results[0].final
             print response.results[0].alternatives[0].transcript
