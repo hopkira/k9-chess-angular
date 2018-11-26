c41c1212a89a09d68ea06b3b6bebcf900fd1eb42
hopkira <hopkira@googlemail.com>
2018-11-26 20:12:55 +0000

    Backing out redis change

--- js/app.js
+++ js/app.js
@@ -9,17 +9,6 @@
 
 var mjpeg_img;
 
-var redis = require('redis');
-var redisClient = redis.createClient({host : 'localhost', port : 6379});
-
-redisClient.on('ready',function() {
- console.log("Redis is ready");
-});
-
-redisClient.on('error',function() {
- console.log("Error in Redis");
-});
-
 function reload_img() {
     mjpeg_img.src = "cam_pic.php?time=" + new Date().getTime();
 }
