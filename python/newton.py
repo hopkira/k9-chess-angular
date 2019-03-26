af44f47a3a96a18dfd4551df48989be6cde9be3e
hopkira <hopkira@googlemail.com>
2019-03-26 19:48:05 +0000

    Slow please

--- python/newton.py
+++ python/newton.py
@@ -16,9 +16,9 @@ rc.Open()
 rc_address = 0x80
 m1_qpps = 1762
 m2_qpps = 1050
-acceleration = 100
-speed = 400
-distance = 1000
+acceleration = 5
+speed = 10
+distance = 100
 # Get roboclaw version to test if is attached
 version = rc.ReadVersion(rc_address)
 # Set PID variables to those required by K9
