58f1da93dcda93e653c1db9a598e5a5e4c28cf03
hopkira <hopkira@googlemail.com>
2019-03-26 16:40:14 +0000

    Revised test

--- python/newton.py
+++ python/newton.py
@@ -18,7 +18,7 @@ m1_qpps = 1762
 m2_qpps = 1050
 acceleration = 250
 speed = 500
-distance = 1000
+distance = 5000
 # Get roboclaw version to test if is attached
 version = rc.ReadVersion(rc_address)
 # Set PID variables to those required by K9
@@ -29,53 +29,60 @@ rc.ResetEncoders(rc_address)
 
 
 def displayspeed():
-	enc1 = rc.ReadEncM1(rc_address)
-	enc2 = rc.ReadEncM2(rc_address)
-	speed1 = rc.ReadSpeedM1(rc_address)
-	speed2 = rc.ReadSpeedM2(rc_address)
+    enc1 = rc.ReadEncM1(rc_address)
+    enc2 = rc.ReadEncM2(rc_address)
+    speed1 = rc.ReadSpeedM1(rc_address)
+    speed2 = rc.ReadSpeedM2(rc_address)
 
-	print("Encoder1:"),
-	if(enc1[0]==1):
-		print enc1[1],
-		print format(enc1[2],'02x'),
-	else:
-		print "failed",
-	print "Encoder2:",
-	if(enc2[0]==1):
-		print enc2[1],
-		print format(enc2[2],'02x'),
-	else:
-		print "failed " ,
-	print "Speed1:",
-	if(speed1[0]):
-		print speed1[1],
-	else:
-		print "failed",
-	print("Speed2:"),
-	if(speed2[0]):
-		print speed2[1]
-	else:
-		print "failed "
+    print("Encoder1:"),
+    if(enc1[0] == 1):
+        print enc1[1],
+        print format(enc1[2],'02x'),
+    else:
+        print "failed",
+    print "Encoder2:",
+    if(enc2[0] == 1):
+        print enc2[1],
+        print format(enc2[2],'02x'),
+    else:
+        print "failed ",
+    print "Speed1:",
+    if(speed1[0]):
+        print speed1[1],
+    else:
+        print "failed",
+    print("Speed2:"),
+    if(speed2[0]):
+        print speed2[1]
+    else:
+        print "failed "
 
 
 rc.SpeedAccelDistanceM1M2(address=rc_address,
-                          accel=acceleration,
+                          accel=int(acceleration),
                           speed1=int(speed),
                           distance1=int(distance),
                           speed2=int(speed),
                           distance2=int(distance),
                           buffer=int(0))
-print "Speed reached..."
-displayspeed()
 rc.SpeedAccelDistanceM1M2(address=rc_address,
-                          accel=acceleration,
+                          accel=int(acceleration),
                           speed1=int(-1),
-                          distance1=int(0),
+                          distance1=int(distance),
                           speed2=int(-1),
-                          distance2=int(0),
+                          distance2=int(distance),
                           buffer=int(1))
-print "Should have decelerated..."
-displayspeed()
-time.sleep(0.5)
-print "Should have stopped..."
-displayspeed()
+
+buffers = (0, 0, 0)
+while(buffers[1] != 0x80 and buffers[2] != 0x80):
+    displayspeed()
+    buffers = rc.ReadBuffers(address)
+    time.sleep(0.1)
+
+rc.SpeedAccelDistanceM1M2(address=rc_address,
+                          accel=int(0),
+                          speed1=int(0),
+                          distance1=int(0),
+                          speed2=int(0),
+                          distance2=int(0),
+                          buffer=int(0))
