0e77fc1726f42d7de4ec3778647ba740789cbe3d
hopkira <hopkira@googlemail.com>
2019-03-26 20:52:33 +0000

    Retuned PID

--- python/newton.py
+++ python/newton.py
@@ -14,18 +14,19 @@ from roboclaw import Roboclaw
 rc = Roboclaw("/dev/roboclaw", 115200)
 rc.Open()
 rc_address = 0x80
-m1_qpps = 1762
-m2_qpps = 1050
+m1_qpps = 1570
+m2_qpps = 1138
 acceleration = 25
 speed = 50
 distance = 138
 # Get roboclaw version to test if is attached
 version = rc.ReadVersion(rc_address)
 # Set PID variables to those required by K9
-rc.SetM1VelocityPID(rc_address, 8.55, 2.21, 0, m1_qpps)
-rc.SetM2VelocityPID(rc_address, 8.96, 2.82, 0, m2_qpps)
+rc.SetM1VelocityPID(rc_address, 6.04, 1.55, 0, m1_qpps)
+rc.SetM2VelocityPID(rc_address, 6.63, 2.11, 0, m2_qpps)
 # Zero the motor encoders
 rc.ResetEncoders(rc_address)
+
 
 def displayspeed():
     enc1 = rc.ReadEncM1(rc_address)
@@ -50,8 +51,8 @@ def displayspeed():
     else:
         print "failed "
 
-base_time = time.time()
 
+base_time = time.time()
 rc.SpeedAccelDistanceM1M2(address=rc_address,
                           accel=int(acceleration),
                           speed1=int(speed),
@@ -75,8 +76,8 @@ while(buffers[1] != 0x80 and buffers[2] != 0x80):
     # time.sleep(0.01)
 print "=== all commands complete =="
 
-rc.ForwardBackwardM1(rc_address, 64)	# Stopped
-rc.ForwardBackwardM2(rc_address, 64)	# Stopped
+rc.ForwardBackwardM1(rc_address, 64)  # Stopped
+rc.ForwardBackwardM2(rc_address, 64)  # Stopped
 
 count = 0
 while (count < 50):
