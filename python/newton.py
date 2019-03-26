7641b0db9f889b365774f4ee77b3eadf1bc1c3e2
hopkira <hopkira@googlemail.com>
2019-03-26 15:56:13 +0000

    New test program

---
+++ python/newton.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Turn on the spot
+#
+# authored by Richard Hopkins March 2019
+#
+# Licensed under The Unlicense, so free for public domain use
+#
+# This program moves K9 in a circle
+
+import math
+import time
+from roboclaw import Roboclaw
+rc = Roboclaw("/dev/roboclaw", 115200)
+rc.Open()
+rc_address = 0x80
+m1_qpps = 1762
+m2_qpps = 1050
+acceleration = 25
+speed = 50
+distance = 100
+# Get roboclaw version to test if is attached
+version = rc.ReadVersion(rc_address)
+# Set PID variables to those required by K9
+rc.SetM1VelocityPID(rc_address, 8.55, 2.21, 0, m1_qpps)
+rc.SetM2VelocityPID(rc_address, 8.96, 2.82, 0, m2_qpps)
+# Zero the motor encoders
+rc.ResetEncoders(rc_address)
+rc.SpeedAccelDistanceM1M2(address=rc_address,
+                          accel=acceleration,
+                          speed1=int(speed),
+                          distance1=int(distance),
+                          speed2=int(speed),
+                          distance2=int(distance),
+                          buffer=int(1))
+rc.SpeedAccelDistanceM1M2(address=rc_address,
+                          accel=655360,
+                          speed1=int(-1),
+                          distance1=int(0),
+                          speed2=int(-1),
+                          distance2=int(0),
+                          buffer=int(0))
+time.sleep(0.5)
+rc.SpeedAccelDistanceM1M2(address=rc_address,
+                          accel=0,
+                          speed1=int(0),
+                          distance1=int(0),
+                          speed2=int(0),
+                          distance2=int(0),
+                          buffer=int(0))
