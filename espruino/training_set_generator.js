2fa220a3843491043e209571883180226dffd0fd
Richard Hopkins <hopkira@googlemail.com>
2020-02-08 17:23:43 +0000

    Training set generator

---
+++ espruino/training_set_generator.js
@@ -0,0 +1,38 @@
+/*
+
+Ultrasonic training set generator
+for Espruino and Synaptic neural net
+
+Will direct output to /dev/ttyESPFollow
+
+Published under The Unlicense
+Richard Hopkins, 2nd February 2020
+
+*/
+
+// Define Espruino pins for PWM Servo and Ultrasonic Sensors
+var FRONT_LEFT = A2;
+var FRONT_RIGHT = A3;
+var LEFT = A4;
+var RIGHT = A5;
+var BACK = A6;
+
+var ON = 1;
+var OFF = 0;
+
+function makeReading() {
+    digitalWrite(LED1, ON);
+    front_left = analogRead(FRONT_LEFT);
+    front_right = analogRead(FRONT_RIGHT);
+    left = analogRead(LEFT);
+    right = analogRead(RIGHT);
+    back = analogRead(BACK);
+    message = '"input":['+front_left+","+front_right+","+left+","+right+","+back+"]\n";
+    USB.write(message);
+    digitalWrite(LED1, OFF);
+    }
+
+function onInit(){
+  USB.setup(115200,{bytesize:8,stopbits:1});
+  setInterval(makeReading,50);
+}
