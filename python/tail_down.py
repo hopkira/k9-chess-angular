39868fb6fd9d5387aa5cf4fd44f2d71e1eff1d59
Richard Hopkins <hopkira@googlemail.com>
2022-01-05 21:30:49 +0000

    Update to tail up and down
    Refreshed library

--- python/tail_down.py
+++ python/tail_down.py
@@ -1,10 +1,20 @@
-# Import the PCA9685 module.
-import Adafruit_PCA9685
-import time
  # ===========================================================================
  # Simple routine to make K9's tail go down
  # ===========================================================================
-pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(60)		# set frequency to 60 Hz
 
-pwm.set_pwm(4, 0, 370)	# tail down
+import time
+import board
+import busio
+import adafruit_pca9685
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c)
+pca.frequency = 60
+
+pca.channels[4].duty_cycle = 5921	# tail down
+
+# 270 -> 4321
+# 320 -> 5121
+# 325 -> 5201
+# 350 -> 5601
+# 370 -> 5921
+# 440 -> 7042
