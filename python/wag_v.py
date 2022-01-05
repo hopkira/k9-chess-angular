92ffa17f5b910bf420c453b2c9dd956298e82e01
Richard Hopkins <hopkira@googlemail.com>
2022-01-05 21:22:22 +0000

    Update wag_v.py
    Update to new library

--- python/wag_v.py
+++ python/wag_v.py
@@ -1,20 +1,33 @@
+# ===========================================================================
+# Simple routine to make K9's tail wag up and down
+# ===========================================================================
 # Import the PCA9685 module.
-import Adafruit_PCA9685
 import time
+import board
+import busio
+import adafruit_pca9685
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c)
+pca.frequency = 60
 
- # ===========================================================================
- # Simple routine to make K9's tail wag up and down
- # ===========================================================================
-pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(60)		# set frequency to 60 Hz
-
-pwm.set_pwm(5, 0, 350)	# tail centre horizontally
+pca.channels[5].duty_cycle = 5601	# tail left
+# pwm.set_pwm(5, 0, 350)	# tail centre horizontally
 
 count= 0
 while count < 4:
-    pwm.set_pwm(4, 0, 370)	# tail up
+    pca.channels[4].duty_cycle = 5921	# tail left
+    # pwm.set_pwm(4, 0, 370)	# tail up
     time.sleep(0.25)
-    pwm.set_pwm(4, 0, 270)	# tail down
+    pca.channels[4].duty_cycle = 4321	# tail left
+    # pwm.set_pwm(4, 0, 270)	# tail down
     time.sleep(0.25)
     count +=1
-pwm.set_pwm(4, 0, 350)	# tail centre
+pca.channels[4].duty_cycle = 5601	# tail left
+# pwm.set_pwm(4, 0, 350)	# tail centre
+
+# 270 -> 4321
+# 320 -> 5121
+# 325 -> 5201
+# 350 -> 5601
+# 370 -> 5921
+# 440 -> 7042
