2691e69aa12cfe3939202aa0abe63eba894fc5b6
Richard Hopkins <hopkira@googlemail.com>
2022-01-08 21:41:32 +0000

    Create back_lights.py

---
+++ python/back_lights.py
@@ -0,0 +1,20 @@
+ # ===========================================================================
+ # Simple routine to make K9's tail go up
+ # ===========================================================================
+
+import time
+import board
+import busio
+import adafruit_pca9685
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c)
+pca.frequency = 60
+
+pca.channels[4].duty_cycle = 4321	# tail up
+
+# 270 -> 4321
+# 320 -> 5121
+# 325 -> 5201
+# 350 -> 5601
+# 370 -> 5921
+# 440 -> 7042
